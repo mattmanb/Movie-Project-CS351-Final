3dd698b2965d1047fbb4bd2561e824a3724dd8c5
Matt Barrington <mattbarrington7@gmail.com>
2023-07-25 17:46:44 -0400

    Top movies works now!

--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -1,50 +1,59 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-const Movie = (props) => (
+const Movie = props => (
     <tr>
-        <td><img src={"https://image.tmdb.org/t/p/w300"+props.movie.poster_path} alt="movie poster"/></td>
+        <td><img src={"https://image.tmdb.org/t/p/w300"+props.movie.poster_path}/></td>
         <td>{props.movie.title}</td>
         <td>{props.movie.overview}</td>
         <td>
             <Link to={"/view/"+props.movie._id}>View</Link>
+
         </td>
     </tr>
 )
+
+
 
 export default class TodosList extends Component {
 
     constructor(props) {
         super(props);
         this.state = {todos: [],
-                    movies: []};
+            movies:[]};
         this.hitMovieList();
     }
 
+
     componentDidMount() {
-        axios.get('http://localhost:4000/todos/')
+        axios.get('http://'+window.location.hostname+':4000/todos/')
             .then(response => {
-                console.log(response.data);
-                this.setState({todos: response.data});
+                console.log(response.data)
+                console.log(window.location);
+                this.setState({ todos: response.data });
             })
-            .catch(function (error) {
+            .catch(function (error){
                 console.log(error);
-            });
+            })
     }
 
     todoList() {
-        return this.state.todos.map(function(currentTodo, i) {
-            return <Movie movie={currentTodo} key={i} />;
-        });
-    }
 
+        return this.state.todos.map(function(currentTodo, i){
+            return <Movie movie={currentTodo} key={i} />;
+        })
+
+    }
     async hitMovieList() {
-        const response = axios.get(
+        const response = await axios.get(
             "https://api.themoviedb.org/3/movie/popular?api_key=30e5cc4bad6f7de3f9215805730d8a4f&language=en-US&page=1"
         );
         console.log(response.data);
-        this.setState({movies: response.data.results});
+        this.setState({movies: response.data.results})
+
+
+
     }
     async addMovie(e){
         e.preventDefault();
@@ -61,9 +70,12 @@ export default class TodosList extends Component {
         console.log(movie);
         axios.post('http://localhost:4000/todos/add', movie)
             .then(res => console.log(res.data));
+
+
     }
 
     render() {
+
         return (
             <div>
                 <div>
@@ -79,7 +91,7 @@ export default class TodosList extends Component {
                         this.state.movies.map((movie, index) => {
                             return (
                                 <tr>
-                                    <td><img src={"https://image.tmdb.org/t/p/w300"+movie.poster_path} alt="movie poster"/></td>
+                                    <td><img src={"https://image.tmdb.org/t/p/w300"+movie.poster_path}/></td>
                                     <td>{movie.title}</td>
                                     <td>{movie.overview}</td>
                                     <td>
@@ -95,22 +107,20 @@ export default class TodosList extends Component {
                     </table>
                 </div>
                 <div>
-                    <h3>Movie List</h3>
-                    <table className="table table-striped" style={{ marginTop: 20 }}>
-                        <thead>
-                            <tr>
-                                <th>Poster</th>
-                                <th>Title</th>
-                                <th>Description</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            { this.todoList() }
-                        </tbody>
-                    </table>
-                </div>
+                <h3>Movie List</h3>
+                <table className="table table-striped" style={{ marginTop: 20 }} >
+                    <thead>
+                    <tr>
+
+                    </tr>
+                    </thead>
+                    <tbody>
+                    { this.todoList() }
+                    </tbody>
+                </table>
             </div>
+            </div>
+
         )
     }
 }
